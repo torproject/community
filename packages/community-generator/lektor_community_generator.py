95912525f02076a7892bcfed0d17bf5cef18e7c4
kez <kez@torproject.org>
2022-11-21 14:25:57 -0800

    Finish proof-of-concept language filter

--- packages/community-generator/lektor_community_generator.py
+++ packages/community-generator/lektor_community_generator.py
@@ -2,6 +2,8 @@
 import json
 import os
 
+from urllib.parse import quote_plus
+
 from lektor.pluginsystem import Plugin
 
 
@@ -11,6 +13,8 @@ class CommunityGeneratorPlugin(Plugin):
 
     contents_lr_tmpl = '''
 section: {section}
+---
+_model: sortby_resources
 ---
 section_id: {section_id}
 ---
@@ -27,6 +31,8 @@ cta: {cta}
 key: {key}
 ---
 html: {html}
+---
+sortby_resources: {sortby_resources}
 ---
 body:
 
@@ -62,6 +68,7 @@ body:
             'cta': '',
             'key': '',
             'html': '',
+            'sortby_resources': '',
             'body': '',
         }
 
@@ -69,27 +76,27 @@ body:
 
         return to_format.format(**formatting)
 
+    def _get_resource_langs(self):
+        """Return a set of every language used by at least one resource."""
+        duplicated_languages = []
+        for resource in self.resources.values():
+            language_list = resource.get('languages', [])
+            duplicated_languages.extend(language_list)
+
+        # flatten
+        return set(duplicated_languages)
+
     def generate_files(self):
         """Generate contents.lr files for the sortby fields: topics, languages, author."""
         with open(os.path.join(self.env.project.tree, 'databags', 'community-training-materials.json'), 'r') as f:
-            resources = json.load(f)
+            self.resources = json.load(f)
 
         # TODO: topics aren't in the databag yet. the below line won't work if the "topic" key is a list, only string
         # gather all the different resources' topics, filter out the `None`s, use a set for uniqueness
         # topics = set(filter(map(lambda resource: resource.get('topic'))))
-        duplicated_languages = []
-        for resource in resources.values():
-            language_list = resource.get('languages')
+        languages = self._get_resource_langs()
 
-            if not language_list:
-                continue
-
-            duplicated_languages.extend(duplicated_languages)
-
-        # flatten
-        languages = set(duplicated_languages)
-
-        authors = set(filter(None, map(lambda resource: resource.get('author'), resources.values())))
+        authors = set(filter(None, map(lambda resource: resource.get('author'), self.resources.values())))
 
 
         # lektor won't render a page if it doesn't have a parent contents.lr
@@ -101,11 +108,30 @@ body:
         # generate sortby/language
         self._generate_file_helper(f'training/resources/sortby/language', self._format_default(self.contents_lr_tmpl))
         for language in languages:
-            self._generate_file_helper(f'training/resources/sortby/language/{language.lower()}', self._format_default(self.contents_lr_tmpl, template='layout.html', html='empty.html', body='# test'))
+            sortby_resources = filter(lambda resource: language.lower() in [lang.lower() for lang in resource[1].get('languages')], self.resources.items())
+            self._generate_file_helper(
+                f'training/resources/sortby/language/{quote_plus(language).lower()}',
+                self._format_default(
+                    self.contents_lr_tmpl,
+                    section='Training',
+                    color='primary',
+                    template='layout.html',
+                    title='Training Resources',
+                    html='resources-sortby.html',
+                    sortby_resources=json.dumps(dict(sortby_resources)),
+                    body='''
+Our Community team delivers digital security training about Tor to human rights defenders, journalists, activists and marginalized communities around the world.
+To request a Tor training for your organization or community, please contact us and send an email to [training at torproject.org](mailto:training@torproject.org).
+Or, if you want to teach your community about Tor, these training materials are for you!
+                    '''
+                ))
 
     def on_setup_env(self, **extra):
         """Generate files when the lektor process starts."""
         self.generate_files()
+        self.env.jinja_env.globals['json_loads'] = json.loads
+        self.env.jinja_env.globals['get_resource_langs'] = self._get_resource_langs
+        self.env.jinja_env.globals['quote_plus'] = quote_plus
 
     def on_server_spawn(self, **extra):
         """Generate files when the dev server restarts."""
