4eefe600b27b6a6b360111f2f5bb50273293b83a
kez <kez@torproject.org>
2022-12-13 12:57:26 -0800

    initial history handling for js sorting

--- assets/js/resources_page.js
+++ assets/js/resources_page.js
@@ -18,6 +18,9 @@ const translatedLangColon = document.currentScript.getAttribute('lang-colon');
 const translatedAuthor = document.currentScript.getAttribute('author');
 const translatedAuthorColon = document.currentScript.getAttribute('author-colon');
 
+// set the initial state, for our history api manipulation
+history.replaceState(resourceFilters, '', window.location.pathname);
+
 function setTopic(ev) {
   ev.preventDefault();
 
@@ -66,25 +69,46 @@ function setAuthor(ev) {
   author_dropdown_button.innerText = newText;
 }
 
-function applyFilters(topic, lang, author) {
+function applyFilters(topic, lang, author, modifyHistory=true) {
   const hide = element => element.classList.add('d-none');
   const unhide = element => element.classList.remove('d-none');
 
   resources.forEach(resource => {
     let hidden = false;
     if (topic && !resource.dataset.topics.includes(`:${topic}:`)) {
-        console.log('topic');
       hide(resource);
     } else if (lang && !resource.dataset.langs.includes(`:${lang}:`)) {
-        console.log(`dataset does not include selected lang: :${lang}:`);
       hide(resource);
     } else if (author && resource.dataset.author != author) {
-        console.log('author');
       hide(resource);
     } else {
       unhide(resource);
     }
   });
+
+  if (modifyHistory) {
+    let filters = {'topic': topic, 'lang': lang, 'author': author};
+    setNewSortUrl(filters);
+  }
+}
+
+function buildNewUrl(currentUrl, filters) {
+  if (currentUrl.slice(-1) == '/') {
+    currentUrl = currentUrl.slice(0, -1);
+  }
+
+  let baseUrl = currentUrl.split('/').slice(0, -3).join('/');
+
+  let topic = filters.topic || 'none';
+  let lang = filters.lang || 'none';
+  let author = filters.author || 'none';
+
+  return `${baseUrl}/${topic}/${lang}/${author}/`;
+}
+
+function setNewSortUrl(filters) {
+  let newUrl = buildNewUrl(window.location.pathname, filters);
+  history.pushState(filters, '', newUrl);
 }
 
 document.querySelectorAll('.filterApplyButton').forEach(button => {
@@ -92,4 +116,11 @@ document.querySelectorAll('.filterApplyButton').forEach(button => {
   button.addEventListener('click', ev => {
     applyFilters(resourceFilters.topic, resourceFilters.lang, resourceFilters.author);
   });
+});
+
+window.addEventListener('popstate', event => {
+  event.preventDefault();
+  console.log(event);
+  console.log(window.location.pathname);
+  applyFilters(event.state.topic, event.state.lang, event.state.author, false)
 });
